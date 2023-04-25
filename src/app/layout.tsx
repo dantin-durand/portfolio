0130e650543d5c507d4b0fe771d98442d362c651
dantin-durand <durand.dantin.pro@gmail.com>
2023-04-25 11:34:51 +0200

    [func] add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata = {
@@ -14,6 +15,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr-FR !font-mono scroll-smooth">
+      <Head>
+        <title>Dantin Durand - Développeur fullstack</title>
+        <meta name="title" content="Dantin Durand - Développeur fullstack" />
+        <meta
+          name="description"
+          content="Développeur fullstack, spécialisé dans le développement web et mobile."
+        />
+        <meta
+          name="keywords"
+          content="dantin durand, dantin, durand, développeur, développeur web, développeur mobile, développeur fullstack, fullstack, web, mobile, webdesign, web design, web designer, web développeur"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.dantindurand.fr/" />
+        <meta
+          property="og:title"
+          content="Dantin Durand - Développeur fullstack"
+        />
+        <meta
+          property="og:description"
+          content="Développeur fullstack, spécialisé dans le développement web et mobile."
+        />
+        <meta property="og:image" content="/assets/logo.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.dantindurand.fr/" />
+        <meta
+          property="twitter:title"
+          content="Dantin Durand - Développeur fullstack"
+        />
+        <meta
+          property="twitter:description"
+          content="Développeur fullstack, spécialisé dans le développement web et mobile."
+        />
+        <meta property="twitter:image" content="/assets/logo.png" />
+      </Head>
       <body className="bg-[#282A35] text-white">
         {children}
         <Analytics />
